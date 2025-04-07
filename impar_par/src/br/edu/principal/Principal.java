f444d9a5fd3a6d45b93b80c408f655b440875087
João Pedro <141186203+ByJoao1@users.noreply.github.com>
2025-04-07 11:13:14 -0300

    Add files via upload

---
+++ impar_par/src/br/edu/principal/Principal.java
@@ -0,0 +1,15 @@
+package br.edu.principal;
+public class Principal {
+public static void main(String[] args) {
+		int p = 0;
+		int i = 1;
+		System.out.println("PARES - IMPARES");
+		while ( p < 100 && i <100) {
+			System.out.println(p + " - " + i);
+			p += 2;
+			i += 2;
+		}
+}
+}
+	
+
